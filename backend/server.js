017aecae8a5a63cd1089176d62396be57ece7f81
Kshitij <kshitijson4@gmail.com>
2024-07-12 19:11:18 +0530

    Developed User Registration

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+require('dotenv').config({ path: './.env' });
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 const { authUser } = require('./middleware/authenticate');
+const { signup } = require('./Auth/signup')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,13 +18,49 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.post('/api/name', authUser, async (req, res) => {
+// For registering new Users
+app.post('/api/signup', async (req, res) => {
 
-    console.log(req.user)
-    res.send("received")
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const result = await signup(username, email, password);
+
+    if (result === 0) {
+        res.send({
+            status: true,
+            message: "Registered Successfully"
+        })
+    }
+    else if (result === -1) {
+        res.send({
+            status: false,
+            message: "User already Exists"
+        })
+    }
+    else {
+        res.send({
+            status: false,
+            message: "Internal Server Error"
+        })
+    }
 
 })
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
-});
+
+
+const startServer = async () => {
+    app.listen(5000, () => {
+        console.log('Server started on port 5000');
+    });
+}
+
+mongoose.connect(process.env.DB_URL)
+    .then(() => {
+        console.log("Connection successful")
+        return startServer()
+    })
+    .catch((error) => {
+        console.log(error)
+    })
